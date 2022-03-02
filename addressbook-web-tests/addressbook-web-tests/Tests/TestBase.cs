013fc8dd4e361812a09377d567738c3f27762acc
rmikhajlov <metallik1993@gmail.com>
2022-03-02 23:03:57 +0300

    Lection 3.2

--- addressbook-web-tests/addressbook-web-tests/Tests/TestBase.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/TestBase.cs
@@ -13,15 +13,7 @@ namespace WebAddressbookTests
         [SetUp]
         public void SetupTest()
         {
-            app = new ApplicationManager();
-            app.Navigator.GoToHomePage();
-            app.Auth.Login(new AccountData("admin", "secret"));
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            app.Stop();
+            app = ApplicationManager.GetInstance();
         }
     }
 }
