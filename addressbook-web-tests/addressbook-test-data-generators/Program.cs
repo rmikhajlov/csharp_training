4df1e665096edbd2dd286d7357a5965d4ad92027
rmikhajlov <metallik1993@gmail.com>
2022-03-27 20:44:03 +0300

    Task 14

--- addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -12,41 +12,83 @@ namespace addressbook_test_data_generators
     {
         static void Main(string[] args)
         {
-            int count = Convert.ToInt32(args[0]);
-            StreamWriter writer = new StreamWriter(args[1]);
-            string format = args[2];
+            string typeOfData = args[0];
+            int count = Convert.ToInt32(args[1]);
+            StreamWriter writer = new StreamWriter(args[2]);
+            string format = args[3];
 
-            List<GroupData> groups = new List<GroupData>();
-
-            for (int i = 0; i < count; i++)
+            // Генерация групп
+            if (typeOfData == "group")
             {
-                groups.Add(new GroupData(TestBase.GenerateRandomString(10)) {
-                    Header = TestBase.GenerateRandomString(100),
-                    Footer = TestBase.GenerateRandomString(100)
-                });
-            }
+                List<GroupData> groups = new List<GroupData>();
 
-            if (format == "csv")
-            {
-                WriteGroupsToCsvFile(groups, writer);
+                for (int i = 0; i < count; i++)
+                {
+                    groups.Add(new GroupData(TestBase.GenerateRandomString(10))
+                    {
+                        Header = TestBase.GenerateRandomString(100),
+                        Footer = TestBase.GenerateRandomString(100)
+                    });
+                }
+                if (format == "csv")
+                {
+                    WriteGroupsToCsvFile(groups, writer);
+                }
+                else if (format == "xml")
+                {
+                    WriteGroupsToXmlFile(groups, writer);
+                }
+                else if (format == "json")
+                {
+                    WriteGroupsToJsonFile(groups, writer);
+                }
+                else
+                {
+                    System.Console.WriteLine("Unknown format: " + format);
+                }
             } 
-            else if (format == "xml")
+
+            // Генерация контактов
+            else if (typeOfData == "contacts")
             {
-                WriteGroupsToXmlFile(groups, writer);
-            }
-            else if (format == "json")
-            {
-                WriteGroupsToJsonFile(groups, writer);
+                List<ContactData> contacts = new List<ContactData>();
+
+                for (int i = 0; i < count; i++)
+                {
+                    contacts.Add(new ContactData(TestBase.GenerateRandomString(20), TestBase.GenerateRandomString(20)));
+                }
+                if (format == "xml")
+                {
+                    WriteContactsToXmlFile(contacts, writer);
+                }
+                else if (format == "json")
+                {
+                    WriteContactsToJsonFile(contacts, writer);
+                }
+                else
+                {
+                    System.Console.WriteLine("Unknown format: " + format);
+                }
             }
             else
             {
-                System.Console.WriteLine("Unknown format: " + format);
+                System.Console.WriteLine("Unknown type of data: " + typeOfData);
             }
+
 
             writer.Close();
         }
 
-        
+        static void WriteContactsToXmlFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<ContactData>)).Serialize(writer, contacts);
+        }
+
+        static void WriteContactsToJsonFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(contacts, Newtonsoft.Json.Formatting.Indented));
+        }
+
         static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
         {
             foreach (GroupData group in groups)
