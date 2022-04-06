3bcbbe75f1a3a3e215ddd06594b3946aa58c604d
rmikhajlov <metallik1993@gmail.com>
2022-04-07 00:09:32 +0300

    Fixed TestAddingContactToGroup

--- addressbook-web-tests/addressbook-web-tests/Tests/AddingContactToGroupTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/AddingContactToGroupTests.cs
@@ -13,13 +13,44 @@ namespace WebAddressbookTests
         [Test]
         public void TestAddingContactToGroup()
         {
-            GroupData group = GroupData.GetAll()[0];
+            GroupData group;
+            ContactData contact;
+
+            // Если ни одной группы не создано, создаём
+            if (GroupData.GetAll().Count == 0)
+            {
+                app.Groups.Create(new GroupData("new group name", "new group header", "new group footer"));
+            }
+
+            group = GroupData.GetAll()[0];
+
+            // Если ни одного контакта не создано, создаём 
+            if (ContactData.GetAll().Count == 0)
+            {
+                contact = new ContactData("new contact name", "new contact lastname");
+                app.Contacts.Create(contact);
+            }
+
+            // Получаем список контактов группы
             List<ContactData> oldList = group.GetContacts();
-            ContactData contact = ContactData.GetAll().Except(oldList).First();
+
+            // Если все контакты добавлены в группу, удаляем первый контакт из группы.
+            // В дальнейшем используем именно этот удалённый контакт.
+            // И обновляем значение oldList.
+            if (ContactData.GetAll().Except(oldList).Count() == 0)
+            {
+                contact = group.GetContacts().First();
+                app.Contacts.RemoveContactFromGroup(contact, group);
+                oldList = group.GetContacts();
+            // Иначе берём первый контакт из не добавленных в группу.
+            }
+            else
+            {
+                contact = ContactData.GetAll().Except(oldList).First();
+            }
 
             // actions
             app.Contacts.AddContactToGroup(contact, group);
-
             List<ContactData> newList = group.GetContacts();
             oldList.Add(contact);
             oldList.Sort();
