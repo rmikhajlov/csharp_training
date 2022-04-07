c3539a084e63b3066de1cee1d39dfe9ac3ed39af
rmikhajlov <metallik1993@gmail.com>
2022-04-07 23:05:14 +0300

    Lection 8.1

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Tests/GroupCreationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData newGroup = new GroupData()
+            {
+                Name = "new group"
+            };
+
+            app.Groups.Add(newGroup);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
